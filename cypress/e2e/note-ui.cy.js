0ade7bad6f0ace6b2d7925b1c9cd2fd2152b1a16
shiftyDE <106715455+shiftyDE@users.noreply.github.com>
2026-07-27 23:02:22 +0200

    feat: update note UI e2e test suite

--- cypress/e2e/note-ui.cy.js
+++ cypress/e2e/note-ui.cy.js
@@ -2,7 +2,7 @@
 
 describe('Note UI', () => {
 
-  it('should allow entering text in the textarea', () => {
+  it('should display a textarea and save button', () => {
     cy.visit('/')
     // Enter the correct username and password
     cy.get('[aria-label="Benutzername"]').type('test')
@@ -13,12 +13,6 @@ describe('Note UI', () => {
     cy.get('[aria-label="Input note"]').click()
     cy.get('[aria-label="Input note"]').type('Cypress Test')
     cy.get('[aria-label="Input note"]').should('contain.text', 'Cypress Test')
-  })
-
-  it('should allow clicking the save button', () => {
-    cy.visit('/')
     cy.get('[aria-label="Add note"]').click()
-    // Verify the page still loads correctly after click (action triggered)
-    cy.url().should('include', '/')
   })
 })
